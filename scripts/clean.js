1bc191f8ed9d2722b07d75b15e7ce93ad0e83a52
theicarus <shorty2292@googlemail.com>
2016-08-19 18:24:33 +0200

    Fix problems in clean script

--- scripts/clean.js
+++ scripts/clean.js
@@ -5,12 +5,42 @@ let distDir = path.join(__dirname, '../dist');
 
 console.log('Clean ' + distDir);
 
-try {
-  fs.rmdirSync(distDir);
-} catch (err) {
-  if (err.code !== 'ENOENT') {
-    console.error(err.code); 
+function removeDir(dirPath) {
+  let content = fs.readdirSync(dirPath);
+  if (content.length > 0) {
+    for (let i = 0; i < content.length; i++) {
+      removeDirOrFile(path.join(dirPath,content[i]));
+    }
+  }
+  fs.rmdirSync(dirPath);
+}
+
+function removeFile(filePath) {
+  fs.unlinkSync(filePath);
+}
+
+function removeDirOrFile(dirFilePath) {
+  let checkContent = fs.statSync(dirFilePath);
+  if (checkContent.isDirectory()) {
+    removeDir(dirFilePath);
+  } else {
+    removeFile(dirFilePath);
   }
 }
+
+function cleanDir(dirPath) {
+  try {
+    let content = fs.readdirSync(dirPath);
+    for (let i = 0; i < content.length; i++) {
+      removeDirOrFile(path.join(dirPath,content[i]));
+    }
+  } catch (err) {
+    if (err.code !== 'ENOENT') {
+      console.error(err);
+    }
+  }
+}
+
+cleanDir(distDir);
 
 console.log('Cleaned ' + distDir);
